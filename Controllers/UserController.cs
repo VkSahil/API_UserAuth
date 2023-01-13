15f49fc15b2e3b8b3609433604bca0b04fcc7faf
vkSahil <sahilsaharan714@gmail.com>
2023-01-13 11:11:14 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserAuth.Context;
+using UserAuth.Models;
+using UserAuth.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UserAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        public object BCrypt { get; private set; }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]  User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == userObj.Username);
+            if(user==null)
+            {
+                return NotFound(new { Message = "User Not Found" });
+            }
+            if(!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new { Message = "Password is Incorrect" });
+            }
+            user.Token = CreateJwt(user);
+            return Ok(new { 
+            Token=user.Token,
+            Message="Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "User Register!"
+            }); ; 
+            
+        }
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysecret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, "user"),
+                new Claim(ClaimTypes.Name,$"{user.FirstName}{user.LastName}")
+            }) ;
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddSeconds(15),
+                SigningCredentials = credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _authContext.Users.ToListAsync());
+        }
+    }
+}
---
+++ Helpers/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
+
+namespace UserAuth.Helpers
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            string PasswordHash = BCrypt.Net.BCrypt.HashPassword(password);
+            return PasswordHash;
+        }
+
+        public static bool VerifyPassword(string password, string PasswordHash)
+        {
+            bool verified = BCrypt.Net.BCrypt.Verify(password , PasswordHash);
+            return verified;
+        }
+    }
+}
